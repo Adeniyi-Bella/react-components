b3496e656ac7a4c33e74d5eae61f4d49efc4aba7
Adeniyi-Bella <adeniyibella@gmail.com>
2023-02-11 14:27:08 +0100

    boiler template with vit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { MainView } from './components/class_components/main-view/main-view'
+
+ReactDOM.createRoot(document.getElementById('root') ).render(
+  <React.StrictMode>
+    <MainView />
+  </React.StrictMode>,
+)
